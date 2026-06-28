bedf41d345b8cfa495ca76701247c8e1f3c420b6
Md Hussain Ahmed <amdhussain538@gmail.com>
2026-06-28 16:04:49 +0600

    added a new solve code

--- frontend/src/app/api/auth/route.js
+++ frontend/src/app/api/auth/route.js
@@ -1,7 +1,7 @@
  
 import { NextResponse } from 'next/server';
 
-const BACKEND_URL = 'http://localhost:5000/api/auth';
+const BACKEND_URL = 'http://localhost:5001/api/auth';
 
 // const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
